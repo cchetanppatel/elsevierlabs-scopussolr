a48d63594cecd3ddf5e464a3de93e6b4ec3ebc37
Darin McBeath <d.mcbeath@elsevier.com>
2013-10-29 15:44:03 -0400

    Added ScopusBulkLoad

---
+++ load/src/com/elsevier/sqs/ScopusBulkLoad.java
@@ -0,0 +1,93 @@
+package com.elsevier.sqs;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.amazonaws.services.sqs.model.SendMessageBatchRequestEntry;
+import com.elsevier.common.Variables;
+
+/**
+ * 
+ * @author Darin McBeath
+ *
+ */
+public class ScopusBulkLoad {
+
+	// Format for the timestamp
+	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss a");
+	
+	public static void main(String[] args) {
+		
+		// Must be one parameter
+		if (args.length != 2 && args.length != 3) {
+			System.err.println("Two parameters are required.  The third parameter is optional.");
+			System.err.println("First parameter is the file containing the Scopus S3 keys.");
+			System.err.println("Second parameter is the version to use.");
+			System.err.println("Third parameter is the number of keys to process.");
+	        System.exit(1);
+		}
+
+    	
+    	System.out.println("*** Start " + sdf.format(new Date()));
+    	int ctr = 0;
+    	try {
+    		
+    		BufferedReader br = new BufferedReader(new FileReader(args[0]));
+    		String line;
+    		List<SendMessageBatchRequestEntry> batch = new ArrayList<SendMessageBatchRequestEntry>();
+
+			 String action = "a";
+			 String prefix = "pfx";
+			 String version = args[1];
+			 String epoch = args[1];
+			 int numberRecordsToProcess = 0;
+			 if (args.length == 3) {
+				 numberRecordsToProcess = Integer.parseInt(args[2],10);
+			 }
+			 int numProcessed = 0;
+			 
+    		 while((line = br.readLine()) != null && (numberRecordsToProcess == 0 || numProcessed < numberRecordsToProcess)) {
+    			 
+    			 numProcessed++;
+    			 
+    			 // Process a record.  The only thing in a line will be the key.
+    			 String key = line.trim();
+
+    			 
+				 MessageEntryForXMLProcessing entry = new MessageEntryForXMLProcessing(action, Long.parseLong(epoch,10), key, prefix, version);
+				 String jsonEntry = MessageForXMLProcessing.toJson(Variables.S3_XML_BUCKET_NAME, entry);
+				 SendMessageBatchRequestEntry messageEntry = new SendMessageBatchRequestEntry()
+				 													.withMessageBody(jsonEntry)
+				 													.withId(Integer.toString(batch.size()));
+				 batch.add(messageEntry);
+				 ctr++;
+				 
+				 // Output in batches of 10
+    			 if (batch.size() == 10) {
+    				 SimpleQueueService.addMessageBatch(Variables.SQS_QUEUE_NAME, batch);
+    				 batch.clear();
+    				 System.out.println("Messages = '" + ctr + "'");
+    			 }
+    		 }
+    		 
+    		 // Output the last batch if necessary
+    		 if (batch.size() > 0) {
+    			 SimpleQueueService.addMessageBatch(Variables.SQS_QUEUE_NAME, batch);
+    		 }
+
+    		 System.out.println("Messages = '" + ctr + "'");
+    		 System.out.println("*** End " + sdf.format(new Date()));
+    		
+    	} catch (Exception ex) {
+    		
+    		ex.printStackTrace();
+    		
+    	}
+		
+	}
+	
+}
